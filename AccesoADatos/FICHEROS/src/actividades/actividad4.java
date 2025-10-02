d94778a8cee718773af05e5e8fbefed6d23a718a
Laura <laurapigar222@gmail.com>
2025-10-02 09:59:10 +0200

    Actividad 4 Ficheros update

---
+++ AccesoADatos/FICHEROS/src/actividades/actividad4.java
@@ -0,0 +1,6 @@
+package actividades;
+
+public class actividad4 {
+    //Crea un programa que, a partir de un fichero de texto codificado en UTF-8,
+    // genere un fichero de texto codificado en ISO-8859-1 y otro en UTF-16
+}
