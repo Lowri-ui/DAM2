46158fa5e57b532d7f0dbcce451b518ea525339c
Laura <laurapigar222@gmail.com>
2025-10-02 10:17:43 +0200

    Añadida librería de java

--- AccesoADatos/FICHEROS/src/actividades/actividad4.java
+++ AccesoADatos/FICHEROS/src/actividades/actividad4.java
@@ -3,4 +3,7 @@ package actividades;
 public class actividad4 {
     //Crea un programa que, a partir de un fichero de texto codificado en UTF-8,
     // genere un fichero de texto codificado en ISO-8859-1 y otro en UTF-16
+    static void main(String[] args) {
+
+    }
 }
