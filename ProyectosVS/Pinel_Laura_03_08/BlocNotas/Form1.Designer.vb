5d1d7bc1e731ebbd8af11c87ad8c457ed220f7ed
Lowri-ui <laurapigar222@gmail.com>
2025-10-19 18:58:40 +0200

    Actividad 03_08 VB

---
+++ ProyectosVS/Pinel_Laura_03_08/BlocNotas/Form1.Designer.vb
@@ -0,0 +1,296 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Panel1 = New Panel()
+        ToolStrip2 = New ToolStrip()
+        ToolStripComboBox2 = New ToolStripComboBox()
+        ToolStripSeparator11 = New ToolStripSeparator()
+        ToolStripLabel1 = New ToolStripLabel()
+        ToolStripSeparator12 = New ToolStripSeparator()
+        ToolStripLabel2 = New ToolStripLabel()
+        ToolStripSeparator13 = New ToolStripSeparator()
+        ToolStripLabel6 = New ToolStripLabel()
+        ToolStripTextBox2 = New ToolStripTextBox()
+        ToolStripSeparator14 = New ToolStripSeparator()
+        ToolStripButton2 = New ToolStripButton()
+        MenuStrip2 = New MenuStrip()
+        ToolStripMenuItem1 = New ToolStripMenuItem()
+        ToolStripMenuItem2 = New ToolStripMenuItem()
+        ToolStripMenuItem3 = New ToolStripMenuItem()
+        ToolStripMenuItem4 = New ToolStripMenuItem()
+        ToolStripSeparator8 = New ToolStripSeparator()
+        ToolStripMenuItem5 = New ToolStripMenuItem()
+        ToolStripMenuItem6 = New ToolStripMenuItem()
+        ToolStripMenuItem7 = New ToolStripMenuItem()
+        ToolStripMenuItem8 = New ToolStripMenuItem()
+        ToolStripMenuItem9 = New ToolStripMenuItem()
+        ToolStripSeparator9 = New ToolStripSeparator()
+        ToolStripMenuItem10 = New ToolStripMenuItem()
+        ToolStripSeparator10 = New ToolStripSeparator()
+        ToolStripMenuItem11 = New ToolStripMenuItem()
+        ToolStripMenuItem12 = New ToolStripMenuItem()
+        RichTextBox1 = New RichTextBox()
+        Panel1.SuspendLayout()
+        ToolStrip2.SuspendLayout()
+        MenuStrip2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.DimGray
+        Panel1.Controls.Add(ToolStrip2)
+        Panel1.Controls.Add(MenuStrip2)
+        Panel1.Location = New Point(1, 0)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(800, 61)
+        Panel1.TabIndex = 2
+        ' 
+        ' ToolStrip2
+        ' 
+        ToolStrip2.BackColor = Color.DimGray
+        ToolStrip2.Items.AddRange(New ToolStripItem() {ToolStripComboBox2, ToolStripSeparator11, ToolStripLabel1, ToolStripSeparator12, ToolStripLabel2, ToolStripSeparator13, ToolStripLabel6, ToolStripTextBox2, ToolStripSeparator14, ToolStripButton2})
+        ToolStrip2.Location = New Point(0, 24)
+        ToolStrip2.Name = "ToolStrip2"
+        ToolStrip2.Size = New Size(800, 25)
+        ToolStrip2.TabIndex = 3
+        ToolStrip2.Text = "ToolStrip2"
+        ' 
+        ' ToolStripComboBox2
+        ' 
+        ToolStripComboBox2.Name = "ToolStripComboBox2"
+        ToolStripComboBox2.Size = New Size(121, 25)
+        ' 
+        ' ToolStripSeparator11
+        ' 
+        ToolStripSeparator11.Name = "ToolStripSeparator11"
+        ToolStripSeparator11.Size = New Size(6, 25)
+        ' 
+        ' ToolStripLabel1
+        ' 
+        ToolStripLabel1.ForeColor = SystemColors.ButtonFace
+        ToolStripLabel1.Name = "ToolStripLabel1"
+        ToolStripLabel1.Size = New Size(16, 22)
+        ToolStripLabel1.Text = "N"
+        ' 
+        ' ToolStripSeparator12
+        ' 
+        ToolStripSeparator12.Name = "ToolStripSeparator12"
+        ToolStripSeparator12.Size = New Size(6, 25)
+        ' 
+        ' ToolStripLabel2
+        ' 
+        ToolStripLabel2.ForeColor = SystemColors.ButtonFace
+        ToolStripLabel2.Name = "ToolStripLabel2"
+        ToolStripLabel2.Size = New Size(14, 22)
+        ToolStripLabel2.Text = "K"
+        ' 
+        ' ToolStripSeparator13
+        ' 
+        ToolStripSeparator13.ForeColor = SystemColors.ButtonFace
+        ToolStripSeparator13.Name = "ToolStripSeparator13"
+        ToolStripSeparator13.Size = New Size(6, 25)
+        ' 
+        ' ToolStripLabel6
+        ' 
+        ToolStripLabel6.ForeColor = SystemColors.ButtonFace
+        ToolStripLabel6.Name = "ToolStripLabel6"
+        ToolStripLabel6.Size = New Size(53, 22)
+        ToolStripLabel6.Text = "Tamaño:"
+        ' 
+        ' ToolStripTextBox2
+        ' 
+        ToolStripTextBox2.Name = "ToolStripTextBox2"
+        ToolStripTextBox2.Size = New Size(100, 25)
+        ' 
+        ' ToolStripSeparator14
+        ' 
+        ToolStripSeparator14.Name = "ToolStripSeparator14"
+        ToolStripSeparator14.Size = New Size(6, 25)
+        ' 
+        ' ToolStripButton2
+        ' 
+        ToolStripButton2.BackColor = Color.Black
+        ToolStripButton2.DisplayStyle = ToolStripItemDisplayStyle.Image
+        ToolStripButton2.ImageTransparentColor = Color.Magenta
+        ToolStripButton2.Name = "ToolStripButton2"
+        ToolStripButton2.Size = New Size(23, 22)
+        ToolStripButton2.Text = "ToolStripButton1"
+        ' 
+        ' MenuStrip2
+        ' 
+        MenuStrip2.BackColor = Color.DimGray
+        MenuStrip2.Items.AddRange(New ToolStripItem() {ToolStripMenuItem1, ToolStripMenuItem6})
+        MenuStrip2.Location = New Point(0, 0)
+        MenuStrip2.Name = "MenuStrip2"
+        MenuStrip2.Size = New Size(800, 24)
+        MenuStrip2.TabIndex = 2
+        MenuStrip2.Text = "MenuStrip2"
+        ' 
+        ' ToolStripMenuItem1
+        ' 
+        ToolStripMenuItem1.DropDownItems.AddRange(New ToolStripItem() {ToolStripMenuItem2, ToolStripMenuItem3, ToolStripMenuItem4, ToolStripSeparator8, ToolStripMenuItem5})
+        ToolStripMenuItem1.ForeColor = SystemColors.ButtonFace
+        ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        ToolStripMenuItem1.Size = New Size(60, 20)
+        ToolStripMenuItem1.Text = "Archivo"
+        ' 
+        ' ToolStripMenuItem2
+        ' 
+        ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        ToolStripMenuItem2.Size = New Size(146, 22)
+        ToolStripMenuItem2.Text = "Nuevo"
+        ' 
+        ' ToolStripMenuItem3
+        ' 
+        ToolStripMenuItem3.Name = "ToolStripMenuItem3"
+        ToolStripMenuItem3.Size = New Size(146, 22)
+        ToolStripMenuItem3.Text = "Abrir"
+        ' 
+        ' ToolStripMenuItem4
+        ' 
+        ToolStripMenuItem4.Name = "ToolStripMenuItem4"
+        ToolStripMenuItem4.Size = New Size(146, 22)
+        ToolStripMenuItem4.Text = "Guarda como"
+        ' 
+        ' ToolStripSeparator8
+        ' 
+        ToolStripSeparator8.Name = "ToolStripSeparator8"
+        ToolStripSeparator8.Size = New Size(143, 6)
+        ' 
+        ' ToolStripMenuItem5
+        ' 
+        ToolStripMenuItem5.Name = "ToolStripMenuItem5"
+        ToolStripMenuItem5.Size = New Size(146, 22)
+        ToolStripMenuItem5.Text = "Salir"
+        ' 
+        ' ToolStripMenuItem6
+        ' 
+        ToolStripMenuItem6.DropDownItems.AddRange(New ToolStripItem() {ToolStripMenuItem7, ToolStripMenuItem8, ToolStripMenuItem9, ToolStripSeparator9, ToolStripMenuItem10, ToolStripSeparator10, ToolStripMenuItem11, ToolStripMenuItem12})
+        ToolStripMenuItem6.ForeColor = SystemColors.ButtonFace
+        ToolStripMenuItem6.Name = "ToolStripMenuItem6"
+        ToolStripMenuItem6.Size = New Size(49, 20)
+        ToolStripMenuItem6.Text = "Editar"
+        ' 
+        ' ToolStripMenuItem7
+        ' 
+        ToolStripMenuItem7.Name = "ToolStripMenuItem7"
+        ToolStripMenuItem7.Size = New Size(162, 22)
+        ToolStripMenuItem7.Text = "Copiar"
+        ' 
+        ' ToolStripMenuItem8
+        ' 
+        ToolStripMenuItem8.Name = "ToolStripMenuItem8"
+        ToolStripMenuItem8.Size = New Size(162, 22)
+        ToolStripMenuItem8.Text = "Cortar"
+        ' 
+        ' ToolStripMenuItem9
+        ' 
+        ToolStripMenuItem9.Name = "ToolStripMenuItem9"
+        ToolStripMenuItem9.Size = New Size(162, 22)
+        ToolStripMenuItem9.Text = "Pegar"
+        ' 
+        ' ToolStripSeparator9
+        ' 
+        ToolStripSeparator9.Name = "ToolStripSeparator9"
+        ToolStripSeparator9.Size = New Size(159, 6)
+        ' 
+        ' ToolStripMenuItem10
+        ' 
+        ToolStripMenuItem10.Name = "ToolStripMenuItem10"
+        ToolStripMenuItem10.Size = New Size(162, 22)
+        ToolStripMenuItem10.Text = "Seleccionar todo"
+        ' 
+        ' ToolStripSeparator10
+        ' 
+        ToolStripSeparator10.Name = "ToolStripSeparator10"
+        ToolStripSeparator10.Size = New Size(159, 6)
+        ' 
+        ' ToolStripMenuItem11
+        ' 
+        ToolStripMenuItem11.Name = "ToolStripMenuItem11"
+        ToolStripMenuItem11.Size = New Size(162, 22)
+        ToolStripMenuItem11.Text = "Deshacer"
+        ' 
+        ' ToolStripMenuItem12
+        ' 
+        ToolStripMenuItem12.Name = "ToolStripMenuItem12"
+        ToolStripMenuItem12.Size = New Size(162, 22)
+        ToolStripMenuItem12.Text = "Rehacer"
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Location = New Point(32, 88)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(747, 332)
+        RichTextBox1.TabIndex = 3
+        RichTextBox1.Text = ""
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(RichTextBox1)
+        Controls.Add(Panel1)
+        Name = "Form1"
+        Text = "Form1"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        ToolStrip2.ResumeLayout(False)
+        ToolStrip2.PerformLayout()
+        MenuStrip2.ResumeLayout(False)
+        MenuStrip2.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents ToolStrip2 As ToolStrip
+    Friend WithEvents ToolStripComboBox2 As ToolStripComboBox
+    Friend WithEvents ToolStripSeparator11 As ToolStripSeparator
+    Friend WithEvents ToolStripLabel1 As ToolStripLabel
+    Friend WithEvents ToolStripSeparator12 As ToolStripSeparator
+    Friend WithEvents ToolStripLabel2 As ToolStripLabel
+    Friend WithEvents ToolStripSeparator13 As ToolStripSeparator
+    Friend WithEvents ToolStripLabel6 As ToolStripLabel
+    Friend WithEvents ToolStripTextBox2 As ToolStripTextBox
+    Friend WithEvents ToolStripSeparator14 As ToolStripSeparator
+    Friend WithEvents ToolStripButton2 As ToolStripButton
+    Friend WithEvents MenuStrip2 As MenuStrip
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem3 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem4 As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator8 As ToolStripSeparator
+    Friend WithEvents ToolStripMenuItem5 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem6 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem7 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem8 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem9 As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator9 As ToolStripSeparator
+    Friend WithEvents ToolStripMenuItem10 As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator10 As ToolStripSeparator
+    Friend WithEvents ToolStripMenuItem11 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem12 As ToolStripMenuItem
+    Friend WithEvents RichTextBox1 As RichTextBox
+
+End Class
