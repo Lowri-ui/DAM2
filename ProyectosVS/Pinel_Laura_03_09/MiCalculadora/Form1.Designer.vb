84813825b47d2cab443e0af4b7e873c56fc43c0b
Laura <laurapigar222@gmail.com>
2025-10-27 13:44:23 +0100

    Actividad 03_09 VB

---
+++ ProyectosVS/Pinel_Laura_03_09/MiCalculadora/Form1.Designer.vb
@@ -0,0 +1,350 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Button15 = New Button()
+        Button16 = New Button()
+        Button17 = New Button()
+        Button18 = New Button()
+        Button19 = New Button()
+        Button20 = New Button()
+        Button21 = New Button()
+        Button22 = New Button()
+        Button23 = New Button()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Anchor = AnchorStyles.Top
+        TextBox1.Font = New Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox1.Location = New Point(23, 37)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(630, 27)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button1.Location = New Point(99, 95)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(70, 72)
+        Button1.TabIndex = 1
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button2.Location = New Point(195, 95)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(70, 72)
+        Button2.TabIndex = 2
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button3.Location = New Point(401, 95)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(70, 72)
+        Button3.TabIndex = 4
+        Button3.Text = "Button3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button4.Location = New Point(296, 95)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(70, 72)
+        Button4.TabIndex = 3
+        Button4.Text = "Button4"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button5.Location = New Point(499, 95)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(70, 72)
+        Button5.TabIndex = 5
+        Button5.Text = "Button5"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button6.Location = New Point(499, 191)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(70, 72)
+        Button6.TabIndex = 10
+        Button6.Text = "Button6"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button7.Location = New Point(401, 191)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(70, 72)
+        Button7.TabIndex = 9
+        Button7.Text = "Button7"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button8.Location = New Point(296, 191)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(70, 72)
+        Button8.TabIndex = 8
+        Button8.Text = "Button8"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button9.Location = New Point(195, 191)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(70, 72)
+        Button9.TabIndex = 7
+        Button9.Text = "Button9"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button10.Location = New Point(99, 191)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(70, 72)
+        Button10.TabIndex = 6
+        Button10.Text = "Button10"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button11
+        ' 
+        Button11.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button11.Location = New Point(499, 284)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(70, 72)
+        Button11.TabIndex = 15
+        Button11.Text = "Button11"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' Button12
+        ' 
+        Button12.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button12.Location = New Point(401, 284)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(70, 72)
+        Button12.TabIndex = 14
+        Button12.Text = "Button12"
+        Button12.UseVisualStyleBackColor = True
+        ' 
+        ' Button13
+        ' 
+        Button13.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button13.Location = New Point(296, 284)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(70, 72)
+        Button13.TabIndex = 13
+        Button13.Text = "Button13"
+        Button13.UseVisualStyleBackColor = True
+        ' 
+        ' Button14
+        ' 
+        Button14.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button14.Location = New Point(195, 284)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(70, 72)
+        Button14.TabIndex = 12
+        Button14.Text = "Button14"
+        Button14.UseVisualStyleBackColor = True
+        ' 
+        ' Button15
+        ' 
+        Button15.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button15.Location = New Point(99, 284)
+        Button15.Name = "Button15"
+        Button15.Size = New Size(70, 72)
+        Button15.TabIndex = 11
+        Button15.Text = "Button15"
+        Button15.UseVisualStyleBackColor = True
+        ' 
+        ' Button16
+        ' 
+        Button16.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button16.Location = New Point(499, 376)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(70, 151)
+        Button16.TabIndex = 20
+        Button16.Text = "Button16"
+        Button16.UseVisualStyleBackColor = True
+        ' 
+        ' Button17
+        ' 
+        Button17.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button17.Location = New Point(401, 376)
+        Button17.Name = "Button17"
+        Button17.Size = New Size(70, 72)
+        Button17.TabIndex = 19
+        Button17.Text = "Button17"
+        Button17.UseVisualStyleBackColor = True
+        ' 
+        ' Button18
+        ' 
+        Button18.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button18.Location = New Point(296, 376)
+        Button18.Name = "Button18"
+        Button18.Size = New Size(70, 72)
+        Button18.TabIndex = 18
+        Button18.Text = "Button18"
+        Button18.UseVisualStyleBackColor = True
+        ' 
+        ' Button19
+        ' 
+        Button19.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button19.Location = New Point(195, 376)
+        Button19.Name = "Button19"
+        Button19.Size = New Size(70, 72)
+        Button19.TabIndex = 17
+        Button19.Text = "Button19"
+        Button19.UseVisualStyleBackColor = True
+        ' 
+        ' Button20
+        ' 
+        Button20.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button20.Location = New Point(99, 376)
+        Button20.Name = "Button20"
+        Button20.Size = New Size(70, 72)
+        Button20.TabIndex = 16
+        Button20.Text = "Button20"
+        Button20.UseVisualStyleBackColor = True
+        ' 
+        ' Button21
+        ' 
+        Button21.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button21.Location = New Point(401, 455)
+        Button21.Name = "Button21"
+        Button21.Size = New Size(70, 72)
+        Button21.TabIndex = 24
+        Button21.Text = "Button21"
+        Button21.UseVisualStyleBackColor = True
+        ' 
+        ' Button22
+        ' 
+        Button22.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button22.Location = New Point(296, 455)
+        Button22.Name = "Button22"
+        Button22.Size = New Size(70, 72)
+        Button22.TabIndex = 23
+        Button22.Text = "Button22"
+        Button22.UseVisualStyleBackColor = True
+        ' 
+        ' Button23
+        ' 
+        Button23.Font = New Font("Verdana", 9F, FontStyle.Bold)
+        Button23.Location = New Point(99, 455)
+        Button23.Name = "Button23"
+        Button23.Size = New Size(166, 72)
+        Button23.TabIndex = 22
+        Button23.Text = "Button23"
+        Button23.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(670, 584)
+        Controls.Add(Button21)
+        Controls.Add(Button22)
+        Controls.Add(Button23)
+        Controls.Add(Button16)
+        Controls.Add(Button17)
+        Controls.Add(Button18)
+        Controls.Add(Button19)
+        Controls.Add(Button20)
+        Controls.Add(Button11)
+        Controls.Add(Button12)
+        Controls.Add(Button13)
+        Controls.Add(Button14)
+        Controls.Add(Button15)
+        Controls.Add(Button6)
+        Controls.Add(Button7)
+        Controls.Add(Button8)
+        Controls.Add(Button9)
+        Controls.Add(Button10)
+        Controls.Add(Button5)
+        Controls.Add(Button3)
+        Controls.Add(Button4)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button20 As Button
+    Friend WithEvents Button21 As Button
+    Friend WithEvents Button22 As Button
+    Friend WithEvents Button23 As Button
+
+End Class
