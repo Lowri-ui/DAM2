e2e2ef2219f4bda355850a764f19826e75513229
Laura <laurapigar222@gmail.com>
2025-11-24 12:45:41 +0100

    Actividad 4 UT 6 Mult. Mov.

--- recursosMoviles/RECURSOS (1)/RECURSOS/ListadosYMenus/app/src/main/java/com/example/listadosymenus/MainActividad4.java
+++ recursosMoviles/RECURSOS (1)/RECURSOS/ListadosYMenus/app/src/main/java/com/example/listadosymenus/MainActividad4.java
@@ -1,7 +1,33 @@
 package com.example.listadosymenus;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActividad4 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad4_layout);
 
+        Spinner spinner = findViewById(R.id.spinnerPaises);
+        TextView textViewSeleccion = findViewById(R.id.textViewPaisSeleccionado);
+
+        String[] paises = {"España", "Francia", "Italia", "Portugal", "Alemania", "Bélgica",
+                "Países Bajos", "Suiza", "Austria", "Suecia", "Laura Pinel García"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>( this,
+                R.layout.list_item_paises,
+                R.id.textViewItemPais,
+                paises);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemClickListener((parent, view, position, id) -> {
+            String seleccionado = paises[position];
+            textViewSeleccion.setText("Has seleccionado: " + seleccionado);
+        });
+    }
 }
