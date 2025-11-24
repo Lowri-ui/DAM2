db536ea8b40dc64185b1506a10bd451fb14558b3
Laura <laurapigar222@gmail.com>
2025-11-24 11:29:01 +0100

    Actividad 3 UT 6 Mult Mov.

--- recursosMoviles/RECURSOS (1)/RECURSOS/ListadosYMenus/app/src/main/java/com/example/listadosymenus/MainActividad3.java
+++ recursosMoviles/RECURSOS (1)/RECURSOS/ListadosYMenus/app/src/main/java/com/example/listadosymenus/MainActividad3.java
@@ -3,25 +3,28 @@ package com.example.listadosymenus;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
-import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActividad3 {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActividad3 extends AppCompatActivity {
 //    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actividad3_layout);
 
-        ListView listView = findViewById(R.id.listViewPaises);
-        GridView gridView =
+        GridView gridView = findViewById(R.id.gridViewPaises);
         TextView textViewSeleccion = findViewById(R.id.textViewPaisSeleccionado);
 
         String[] paises = {"España", "Francia", "Italia", "Portugal", "Alemania", "Bélgica",
                 "Países Bajos", "Suiza", "Austria", "Suecia", "Laura Pinel García"};
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_item_paises, R.id.textViewItemPais, paises);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener((parent, view, position, id) -> {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>( this,
+                R.layout.list_item_paises,
+                R.id.textViewItemPais,
+                paises);
+        gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener((parent, view, position, id) -> {
             String seleccionado = paises[position];
             textViewSeleccion.setText("Has seleccionado: " + seleccionado);
         });
