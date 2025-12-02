dad8f96a1b41b558eda24028b9301543fd49a799
Laura <laurapigar222@gmail.com>
2025-12-02 12:21:26 +0100

    BBDD DataGridView VB

---
+++ ProyectosVS/Pinel_Laura_04_01/BBDDGuardarDataGridView/Form1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        PanelTitulo = New Panel()
+        PanelLogo = New Panel()
+        lblTitulo = New Label()
+        PictureBox1 = New PictureBox()
+        TabControl1 = New TabControl()
+        TabPage1 = New TabPage()
+        TabPage2 = New TabPage()
+        PanelTitulo.SuspendLayout()
+        PanelLogo.SuspendLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        TabControl1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' PanelTitulo
+        ' 
+        PanelTitulo.BackColor = Color.FromArgb(CByte(18), CByte(32), CByte(47))
+        PanelTitulo.BorderStyle = BorderStyle.FixedSingle
+        PanelTitulo.Controls.Add(lblTitulo)
+        PanelTitulo.Controls.Add(PanelLogo)
+        PanelTitulo.Dock = DockStyle.Top
+        PanelTitulo.ForeColor = Color.FromArgb(CByte(70), CByte(70), CByte(70))
+        PanelTitulo.Location = New Point(0, 0)
+        PanelTitulo.Name = "PanelTitulo"
+        PanelTitulo.Size = New Size(800, 67)
+        PanelTitulo.TabIndex = 0
+        ' 
+        ' PanelLogo
+        ' 
+        PanelLogo.Controls.Add(PictureBox1)
+        PanelLogo.Dock = DockStyle.Left
+        PanelLogo.Location = New Point(0, 0)
+        PanelLogo.Name = "PanelLogo"
+        PanelLogo.Size = New Size(82, 65)
+        PanelLogo.TabIndex = 0
+        ' 
+        ' lblTitulo
+        ' 
+        lblTitulo.AutoSize = True
+        lblTitulo.Font = New Font("Calibri", 14.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblTitulo.ForeColor = Color.FromArgb(CByte(0), CByte(204), CByte(255))
+        lblTitulo.Location = New Point(97, 27)
+        lblTitulo.Name = "lblTitulo"
+        lblTitulo.Size = New Size(263, 23)
+        lblTitulo.TabIndex = 1
+        lblTitulo.Text = "CONEXIÓN A LA BASE DE DATOS"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Dock = DockStyle.Fill
+        PictureBox1.Image = My.Resources.Resources.base_de_datos
+        PictureBox1.Location = New Point(0, 0)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(82, 65)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 2
+        PictureBox1.TabStop = False
+        ' 
+        ' TabControl1
+        ' 
+        TabControl1.Controls.Add(TabPage1)
+        TabControl1.Controls.Add(TabPage2)
+        TabControl1.Location = New Point(37, 107)
+        TabControl1.Name = "TabControl1"
+        TabControl1.SelectedIndex = 0
+        TabControl1.Size = New Size(738, 295)
+        TabControl1.TabIndex = 1
+        ' 
+        ' TabPage1
+        ' 
+        TabPage1.Location = New Point(4, 24)
+        TabPage1.Name = "TabPage1"
+        TabPage1.Padding = New Padding(3)
+        TabPage1.Size = New Size(730, 267)
+        TabPage1.TabIndex = 0
+        TabPage1.Text = "Cargar"
+        TabPage1.UseVisualStyleBackColor = True
+        ' 
+        ' TabPage2
+        ' 
+        TabPage2.Location = New Point(4, 24)
+        TabPage2.Name = "TabPage2"
+        TabPage2.Padding = New Padding(3)
+        TabPage2.Size = New Size(730, 267)
+        TabPage2.TabIndex = 1
+        TabPage2.Text = "TabPage2"
+        TabPage2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(28), CByte(36), CByte(48))
+        ClientSize = New Size(800, 450)
+        Controls.Add(TabControl1)
+        Controls.Add(PanelTitulo)
+        Name = "Form1"
+        Text = "Form1"
+        PanelTitulo.ResumeLayout(False)
+        PanelTitulo.PerformLayout()
+        PanelLogo.ResumeLayout(False)
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        TabControl1.ResumeLayout(False)
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents PanelTitulo As Panel
+    Friend WithEvents lblTitulo As Label
+    Friend WithEvents PanelLogo As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+
+End Class
